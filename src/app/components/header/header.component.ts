7e4d620aa0cab68a9abedc1e5bb0868ad1837590
Strider2342 <arpad.ivany@gmail.com>
2024-08-31 09:58:21 +0200

    feat(components): separated header components (#1)

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
 
+import { MineCountComponent } from '../mine-count/mine-count.component';
+import { TimerComponent } from '../timer/timer.component';
+import { PlayerIndicatorComponent } from '../player-indicator/player-indicator.component';
+
 @Component({
   selector: 'app-header',
   standalone: true,
+  imports: [MineCountComponent, PlayerIndicatorComponent, TimerComponent],
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
